a55ba9798ba4b4676c0d8e3e1b0e4d65eae7bf60
mayowa86 <mayowasamuel86@gmail.com>
2022-04-08 02:23:59 +0100

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,131 @@
+(function($){
+	'use script';
+	// Scroll Area
+	$(document).ready(function(){
+	    $('.scroll-area').click(function(){
+	      	$('html').animate({
+	        	'scrollTop' : 0,
+	      	},700);
+	      	return false;
+	    });
+	    $(window).on('scroll',function(){
+	      	var a = $(window).scrollTop();
+	      	if(a>400){
+	            $('.scroll-area').slideDown(300);
+	        }else{
+	            $('.scroll-area').slideUp(200);
+	        }
+	    });
+	});
+	// Our Testimonial 
+  	$(".testimonial-full").owlCarousel({
+  		loop:true,
+  		center:false,
+  		items:3,
+  		autoplay: true,
+  		autoplayTimeout:7000,
+  		smartSpeed:700,
+  		responsive:{
+  			0:{
+  				items:1,
+  			},
+  			767 : {
+		        items:2,
+		    },
+  			991 : {
+		        items:3,
+		    }
+  		}
+  	});
+  	/*
+	 * Mobile Menu
+  	*/
+  	//Mobile Menu
+	//===== Mobile Menu
+    $('.mobile-menu-open').on('click', function(){
+        $('.mobile-off-canvas-menu').addClass('open')
+        $('.overlay').addClass('open')
+    });
+    $('.close-mobile-menu').on('click', function(){
+        $('.mobile-off-canvas-menu').removeClass('open')
+        $('.overlay').removeClass('open')
+    });
+    $('.overlay').on('click', function(){
+        $('.mobile-off-canvas-menu').removeClass('open')
+        $('.overlay').removeClass('open')
+    });
+    /*Variables*/
+    var $offCanvasNav = $('.mobile-main-menu'),
+        $offCanvasNavSubMenu = $offCanvasNav.find('.mega-sub-menu, .sub-menu, .submenu-item ');
+    /*Add Toggle Button With Off Canvas Sub Menu*/
+    $offCanvasNavSubMenu.parent().prepend('<span class="mobile-menu-expand"></span>');
+    /*Close Off Canvas Sub Menu*/
+    $offCanvasNavSubMenu.slideUp();
+    /*Category Sub Menu Toggle*/
+    $offCanvasNav.on('click', 'li a, li .mobile-menu-expand, li .menu-title', function(e) {
+        var $this = $(this);
+        if (($this.parent().attr('class').match(/\b(menu-item-has-children|has-children|has-sub-menu)\b/)) && ($this.attr('href') === '#' || $this.hasClass('mobile-menu-expand'))) {
+            e.preventDefault();
+            if ($this.siblings('ul:visible').length) {
+                $this.parent('li').removeClass('active-expand');
+                $this.siblings('ul').slideUp();
+            } else {
+                $this.parent('li').addClass('active-expand');
+                $this.closest('li').siblings('li').find('ul:visible').slideUp();
+                $this.closest('li').siblings('li').removeClass('active-expand');
+                $this.siblings('ul').slideDown();
+            }
+        }
+    });
+	//Sticky Menu
+	$(window).on('scroll',function(){
+		var scroll = $(window).scrollTop();
+		if(scroll < 150){
+			$('.header-bottom').removeClass('sticky');
+		}else{
+			$('.header-bottom').addClass('sticky');
+		}
+	});
+
+	// Counter
+	$('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+	// Google Map
+    function basicmap() {
+        // Basic options for a simple Google Map
+        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+        var mapOptions = {
+            // How zoomed in you want the map to start at (always required)
+            zoom: 11,
+            scrollwheel: false,
+            // The latitude and longitude to center the map (always required)
+            center: new google.maps.LatLng(40.6700, -73.9400), // New York
+            // This is where you would paste any style found on Snazzy Maps.
+            styles: [{"featureType":"all","elementType":"geometry","stylers":[{"color":"#202c3e"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"gamma":0.01},{"lightness":20},{"weight":"1.39"},{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"weight":"0.96"},{"saturation":"9"},{"visibility":"on"},{"color":"#000000"}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"lightness":30},{"saturation":"9"},{"color":"#29446b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"saturation":20}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"lightness":20},{"saturation":-20}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":10},{"saturation":-30}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#193a55"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"saturation":25},{"lightness":25},{"weight":"0.01"}]},{"featureType":"water","elementType":"all","stylers":[{"lightness":-20}]}]
+        };
+        // Get the HTML DOM element that will contain your map 
+        // We are using a div with id="map" seen below in the <body>
+        var mapElement = document.getElementById('contact-map');
+
+        // Create the Google Map using our element and options defined above
+        var map = new google.maps.Map(mapElement, mapOptions);
+
+        // Let's also add a marker while we're at it
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(40.6700, -73.9400),
+            map: map,
+            title: 'Cryptox'
+        });
+    }
+    if ($('#contact-map').length != 0) {
+        google.maps.event.addDomListener(window, 'load', basicmap);
+    }
+    //Preloader
+    $(window).on('load', function(event) {
+        $('#preloader').delay(500).fadeOut(500);
+    });
+
+}(jQuery));
